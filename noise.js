a2f8cefda58b17f19b1d63a78c501f784e8480bb
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-21 13:31:41 -0800

    Notes playing via mouseclick

--- noise.js
+++ noise.js
@@ -1,3 +1,21 @@
+
 $(document).ready( function() {
-  // your code here
+
+  $('.instrument').on('click', 'button', function(event) {
+    if ($(this).hasClass('note c')) {
+      $("#cAudio").get(0).play();
+    } else if ($(this).hasClass('note d')) {
+      $("#dAudio").get(0).play();
+    } else if ($(this).hasClass('note e')) {
+      $("#eAudio").get(0).play();
+    } else if ($(this).hasClass('note f')) {
+      $("#fAudio").get(0).play();
+    } else if ($(this).hasClass('note g')) {
+      $("#gAudio").get(0).play();
+    } else if ($(this).hasClass('note a')) {
+      $("#aAudio").get(0).play();
+    } else {
+      $("#bAudio").get(0).play();
+    }
+  });
 });
